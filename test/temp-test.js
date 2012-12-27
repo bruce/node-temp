5968cd2046e83d72021876b9800fe103471128b2
Bruce Williams <bruce.williams@livingsocial.com>
2012-12-26 21:52:26 -0800

    Add createWriteStream() as requested in #8.

--- test/temp-test.js
+++ test/temp-test.js
@@ -53,6 +53,21 @@ temp.open('bar', function(err, info) {
   util.log("open " + openPath);
 });
 
+
+var stream = temp.createWriteStream('baz');
+assert.ok(stream instanceof fs.WriteStream, "temp.createWriteStream did not invoke the callback with the err and stream object");
+stream.write('foo');
+stream.end();
+assert.ok(existsSync(stream.path), 'temp.createWriteStream did not create a file');
+
+temp.cleanup();
+fs.exists(stream.path, function(exists) {
+  assert.ok(!exists, 'temp.cleanup did not remove the createWriteStream file');
+});
+
+util.log("createWriteStream " + stream.path);
+
+
 for (var i=0; i <= 10; i++) {
   temp.openSync();
 };
